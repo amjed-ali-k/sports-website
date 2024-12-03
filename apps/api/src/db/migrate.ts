6e45b673a6feb21e233f759aae80bfd5cee00bac
amjed-ali-k <amjedmgm@gmail.com>
2024-12-03 11:20:35 +0530

    fix: types

--- apps/api/src/db/migrate.ts
+++ apps/api/src/db/migrate.ts
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/d1";
 import { migrate } from "drizzle-orm/d1/migrator";
-import createAdmin from "../../scripts/create-admin";
 
 export default async function runMigrations(db: D1Database) {
   const drizzleDb = drizzle(db);
@@ -9,7 +8,5 @@ export default async function runMigrations(db: D1Database) {
   await migrate(drizzleDb, { migrationsFolder: "./drizzle" });
   console.log("Migrations complete");
 
-  console.log("Creating initial admin...");
-  await createAdmin(db);
   console.log("Setup complete");
 }
