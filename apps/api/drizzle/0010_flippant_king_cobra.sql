b738a2eaaa6113bf4dfdeec7a2a4062fb4c18c31
amjed-ali-k <amjedmgm@gmail.com>
2024-12-14 21:45:08 +0530

    fix: buld with logs

--- apps/api/drizzle/0010_flippant_king_cobra.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `group_registrations` ADD `name` text;
--- apps/api/drizzle/0011_silent_pyro.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `group_items` ADD `status` text DEFAULT 'scheduled' NOT NULL;--> statement-breakpoint
-ALTER TABLE `group_registrations` ADD `section_id` integer REFERENCES sections(id);--> statement-breakpoint
-ALTER TABLE `items` ADD `status` text DEFAULT 'scheduled' NOT NULL;
